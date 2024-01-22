a1f5838c0bbf29f4acc6cb3ac9f2669fbbbfe7aa
Wilachat Weesakul <waan@Wilachats-MacBook-Pro.local>
2024-01-22 12:06:20 -0500

    added add

--- public/submitData.js
+++ public/submitData.js
@@ -135,9 +135,8 @@ document.addEventListener('DOMContentLoaded', function(req) {
 
     
         // Validate that the start date is today and the end date is no earlier than the day after
-        if (endDate.getTime() >= startDate.getTime() && startDateInput === today) {
+        if (endDate.getTime() > startDate.getTime() && startDate === today) {
            
-          
            
             console.log("check startdate", formattedStartDate)
             console.log("check enddate", formattedEndDate)
